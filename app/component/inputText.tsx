8569257e1d1843466a46add470d9ed12b2c993d2
zefahdzfh <zefakalibata@gmail.com>
2023-08-07 11:42:28 +0700

    7 august notes

---
+++ app/component/inputText.tsx
@@ -0,0 +1,38 @@
+import clsx from "clsx";
+
+interface InputProps {
+  isError?: boolean;
+  messageError?: string;
+  value: number | string;
+}
+
+const InputText: React.FC<
+  InputProps & React.InputHTMLAttributes<HTMLInputElement>
+> = ({ messageError = "wajib di isi", isError = false, value, ...props }) => {
+  return (
+    <section>
+      <input
+        value={value}
+        className={clsx(`w-full h-8 border border-gray-700 rounded px-2`, {
+            "border-gray-700": isError === false,
+            'border-red-500': isError ===true,
+        })}
+        {...props}
+      />
+
+      {isError === true ? (
+        <p className="text-red-500 text-sm">{messageError}</p>
+      ) : (
+        <></>
+      )}
+
+      {/* {isError ? (
+          <p className="text-red-500 font-bold">{messageError}</p>
+        ) : (
+          <></>
+        )} */}
+    </section>
+  );
+};
+
+export default InputText;
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,30 @@
-import Button from "./component/button"
-import Latihan from "./component/latihan"
-import Note from "./component/note"
-function Home(){
-  return(
+import Button from "./component/button";
+import InputText from "./component/inputText";
+import Latihan from "./component/latihan";
+import Note from "./component/note";
+function Home() {
+  return (
     <>
-     <h1>Hello World</h1>
-{/* 
+      <h1>Hello World</h1>
+
+      
+      <Latihan name="izan" age={18} username="izan" isVerified />
+      <Latihan name="zaky" age={15} username="zaky" isVerified={false} />
+
+      <Button title="save" isDisabled />
+      <Button title="cancel" />
+      
+
+      <Note title=" Belajar NextJS" status="success">
+        <div className="">Sedang belajar NextJS</div>
+      </Note>
+
+     <Note title=" Belajar NextJS" status="success">
+     <Button title="cancel"/>
      <Latihan name="zefa" age={15} username="zefa" isVerified={true} />
-     <Latihan name="izan" age={18} username="izan" isVerified  />
-     <Latihan name="zaky" age={15} username="zaky" isVerified={false} /> */}
-
-     <Button title="save" isDisabled/>
-     <Button title="cancel"/>
-
-     <Note title=" Belajar NextJS" status="success">
-      <div className="">Sedang belajar NextJS</div>
      </Note>
-     <Note title=" Belajar NextJS" status="success">
-     <Button title="cancel"/>
-     </Note>
-     <Note title=" Belajar NextJS" status="success">
+
+     <Note title=" Belajar NextJS" status="warning">
       ok
      </Note>
 
@@ -28,9 +33,16 @@ function Home(){
           <h5>latihan</h5>
         </div>
      </Note>
+
+
+     <input name="username" type="text" value={'ok'} />
+     <InputText isError value={'123'} type="password"></InputText>
+     <InputText isError={true} value={'tes'} placeholder="username" type="text"></InputText>
+     <InputText value={237372897398}></InputText>
+
+
     </>
-   
-  )
+  );
 }
 
-export default Home
+export default Home;
